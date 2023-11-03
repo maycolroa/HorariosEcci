77168b9380c8dbc83092b2d499d139e628299b54
maycolroa <lehallo@hotmail.com>
2023-11-03 16:24:41 -0500

    creacion de servicios y coneccion a base de datos

---
+++ Backend/AplicacionHorariosEcci/Models/Custom/AutorizacionRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace AplicacionHorariosEcci.Models.Custom
+{
+    public class AutorizacionRequest
+    {
+        public string NombreUsuario { get; set; }
+        public string Clave { get; set; }
+    }
+}
---
+++ Backend/AplicacionHorariosEcci/Services/IAutorizacionService.cs
@@ -0,0 +1,10 @@
+﻿using AplicacionHorariosEcci.Models.Custom;
+
+
+namespace AplicacionHorariosEcci.Services
+{
+    public interface IAutorizacionService
+    {
+        Task<AutorizacionResponse> DevolverToken(AutorizacionResponse autorizacion);
+    }
+}
